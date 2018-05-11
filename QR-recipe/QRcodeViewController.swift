4b75b204c8b91c8992f063d07ea75f727e854f15
HvrlK <g.v.s.23.07@gmail.com>
2018-05-11 03:07:02 +0300

    Delete ability share QR code.

--- QR-recipe/QRcodeViewController.swift
+++ QR-recipe/QRcodeViewController.swift
@@ -17,7 +17,6 @@ class QRcodeViewController: UIViewController {
     // MARK: - Outlets
     
     @IBOutlet weak var navigationBar: UINavigationBar!
-    @IBOutlet weak var shareButton: UIBarButtonItem!
     @IBOutlet weak var QRcodeImageView: UIImageView!
 
     // MARK: - Methods
@@ -27,8 +26,6 @@ class QRcodeViewController: UIViewController {
         navigationBar.delegate = self
         if let image = image {
             QRcodeImageView.image = image
-        } else {
-            shareButton.isEnabled = false
         }
     }
 
@@ -40,15 +37,6 @@ class QRcodeViewController: UIViewController {
     
     @IBAction func doneButtonTapped(_ sender: UIBarButtonItem) {
         dismiss(animated: true, completion: nil)
-    }
-    
-    @IBAction func shareButtonTapped(_ sender: UIBarButtonItem) {
-        if let image = QRcodeImageView.image {
-            let imageToShare = [ image ]
-            let activityViewController = UIActivityViewController(activityItems: imageToShare, applicationActivities: nil)
-            activityViewController.popoverPresentationController?.sourceView = self.view
-            self.present(activityViewController, animated: true, completion: nil)
-        }
     }
     
 }
